a8ab8eac9a5e0be3260002797b4ba7b20c94c1c9
soumitacodes <karsoumita8@gmail.com>
2025-08-24 19:34:57 +0530

    navbar

---
+++ front-end/src/assets/assets.js
@@ -0,0 +1,29 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import search_icon from './search_icon.png'
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_stars from './rating_stars.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+
+
+export const assets = {
+    logo,
+    basket_icon,
+    search_icon,
+    rating_stars,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon
+}
+
